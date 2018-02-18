0b3414871438c35a6534202e892cee9a2bc9b0a4
javiray619 <34381738+javiray619@users.noreply.github.com>
2018-02-17 20:38:53 -0800

    Add files via upload

--- bag_of_words.py
+++ bag_of_words.py
@@ -1,22 +1,27 @@
-
 from collections import Counter
 import numpy as np
+import pandas as pd
 import re
+import sys
 
 
 def main():
-    words = ["the is black dog the is black dog, cow", "Monkeys are blue and black"]
+    if len(sys.argv) != 2:
+        raise Exception("usage: python train.csv")
+    # inputfile = sys.argv[1]
+    # df = pd.read_csv(inputfile, header = 0)
+    descriptions = df['item_description'].as_matrix()
+    # descriptions = ["The dog is black", "Cows are white"]
     count = 0
     vocabulary = dict()
-    for i in range (0, len(words)):
-        vocabulary, count = build_vocabulary(words[i], count, vocabulary)
-
-    encoding = np.zeros((len(vocabulary), len(words)))
-    for i in range (0, len(words)):
-        encoding[:, i] = create_document_vector(sentenceToArr(words[i]), vocabulary)
-
-    print"encoding: ", encoding
-    print"dictionary: ", vocabulary
+    masterLength = len(descriptions)
+    for i in range (0, masterLength):
+        vocabulary, count = build_vocabulary(descriptions[i], count, vocabulary)
+    df['description_encoding'] = pd.Series(np.zeros(masterLength), index = df.index)
+    encodings = np.zeros((len(vocabulary), len(descriptions)))
+    for i in range (0, len(descriptions)):
+        df.at[i, 'description_encoding'] = create_document_vector(sentenceToArr(descriptions[i]), vocabulary))
+    df.to_csv('train_enc.csv', index = False)
 
 def build_vocabulary(sentence, Count, dictionary):
     # For each word in words vector put in dictionary with index
@@ -42,9 +47,10 @@ def create_document_vector(description, vocabulary):
     # Build vector for sentence coding, at index in vocabulary put word frequency
     word_count = sentence_count(description)
     encode = np.zeros((len(vocabulary)))
-    for word, index in vocabulary.items():
-        if word in word_count:
-            encode[index - 1] = word_count[word]
+    for word, count in word_count.iteritems():
+        if word in vocabulary:
+            index =  vocabulary[word]
+            encode[index - 1] = count
     return encode
 
 

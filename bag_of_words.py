f8ff74bdc0c1507a929d74ab63e5c533382e380f
javiray619 <34381738+javiray619@users.noreply.github.com>
2018-02-19 10:19:08 -0800

    Add files via upload
    Doesn't stringify encodings anymore

--- bag_of_words.py
+++ bag_of_words.py
@@ -15,17 +15,22 @@ def main():
     vocabulary = dict()
     masterLength = len(descriptions)
     for i in range (0, masterLength):
-        vocabulary, count = build_vocabulary(descriptions[i], count, vocabulary)
+        if (pd.isnull(descriptions[i]) == False):
+            vocabulary, count = build_vocabulary(descriptions[i], count, vocabulary)
     encodings = []
     for i in range (0, len(descriptions)):
-        encodings.append(str(create_document_vector(sentenceToArr(descriptions[i]), vocabulary)))
+        if (pd.isnull(descriptions[i]) == False):
+            encodings.append(create_document_vector(sentenceToArr(descriptions[i]), vocabulary))
+        else:
+            print "At iteration ", i, ", no product description was available"
+            encodings.append((np.zeros((len(vocabulary)))))
     df['encodings'] = pd.Series(encodings, index = df.index)
+    # print df['encodings']
     df.to_csv('train_enc.csv', index = False)
 
 def build_vocabulary(sentence, Count, vocabulary):
     # For each word in words vector put in dictionary with index
     words= sentenceToArr(sentence)
-    print words
     count = Count
     for word in words:
         if word in vocabulary:
@@ -36,7 +41,7 @@ def build_vocabulary(sentence, Count, vocabulary):
     return vocabulary, count
 
 def sentenceToArr(sentence):
-    return re.split(' |; |, |\*|\n',sentence)
+    return re.split(' |; |, |\*|\n', sentence)
 
 def sentence_count(sentence):
     # Count occurance of each word in sentence

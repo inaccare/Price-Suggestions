ca28c7c448046a8aaaf448e0a63c70dbdb8dd079
javiray619 <34381738+javiray619@users.noreply.github.com>
2018-02-20 20:23:55 -0800

    Add files via upload
    Instead of outputting a vector of length of courpus, outputting vector indicating indices into corpus of words that show up

--- bag_of_words.py
+++ bag_of_words.py
@@ -17,18 +17,19 @@ def main():
     lengths = [int(masterLength/8), int(masterLength/4), int(3*masterLength/8), int(masterLength/2),int(5*masterLength/8), int(3*masterLength/4), int(7*masterLength/8)]
     for i in range (0, masterLength):
         if (pd.isnull(descriptions[i]) == False):
-            vocabulary, count = build_vocabulary(descriptions[i], count, vocabulary)
+            vocabulary, count = build_vocabulary(descriptions[i].lower(), count, vocabulary)
     encodings = []
     j = 0
     for i in range (0, len(descriptions)):
-        if i == lengths[j]:
-            j = j+ 1
-            print "Currently ", j, "/8 of the way through encoding"
+        if j!=7:
+            if i == lengths[j]:
+                print "Currently ", j+1, "/8 of the way through encoding"
+                j = j+1
         if (pd.isnull(descriptions[i]) == False):
-            encodings.append(create_document_vector(sentenceToArr(descriptions[i]), vocabulary))
+            encodings.append(create_document_vector(sentenceToArr(descriptions[i].lower()), vocabulary))
         else:
             print "At iteration ", i, ", no product description was available"
-            encodings.append((np.zeros((len(vocabulary)))))
+            encodings.append(np.array([]))
     df['encodings'] = pd.Series(encodings, index = df.index)
     # print df['encodings']
     df.to_csv('train_enc.csv', index = False)
@@ -56,12 +57,12 @@ def sentence_count(sentence):
 def create_document_vector(description, vocabulary):
     # Build vector for sentence coding, at index in vocabulary put word frequency
     word_count = sentence_count(description)
-    encode = np.zeros((len(vocabulary)))
+    encode = []
     for word, count in word_count.iteritems():
         if word in vocabulary:
             index =  vocabulary[word]
-            encode[index - 1] = count
-    return encode
+            encode.append(index-1)
+    return np.array(encode)
 
 
 

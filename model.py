88206a5fdcf9f9d6ac72954e3aaed1875a2ddbf4
emmiekehoe <emmiekehoe@gmail.com>
2018-02-23 15:15:04 -0800

    Add files via upload

--- model.py
+++ model.py
@@ -1,4 +1,3 @@
-# Import data
 import pandas as pd
 import re
 import sys
@@ -10,12 +9,11 @@ import matplotlib.pyplot as plt
 import tensorflow as tf
 from tensorflow.python.framework import ops
 import json
-# import tensorflow_utils
-# from tf_utils import load_dataset, random_mini_batches, convert_to_one_hot, predict
+
 
 def main():
     # Usage is as follows: python model.py <train_enc>.csv <test_enc>.csv(optional)
-    # Test variables are dummy variables for now
+
     X_test = []
     Y_test = []
     testCSV = None
@@ -24,7 +22,7 @@ def main():
         trainCSV = sys.argv[1]
     if (len(sys.argv) == 3):
         devCSV = sys.argv[2]
-    trainDF = pd.read_csv(trainCSV, header = 0) # Using this for now so it runs. Take out when training all of them
+    trainDF = pd.read_csv(trainCSV, header = 0)
     X_train, Y_train = getProductEncodingsAndPrices(trainDF)
     devDF = pd.read_csv(devCSV, header = 0)
     X_dev, Y_dev = getProductEncodingsAndPrices(devDF)
@@ -34,23 +32,23 @@ def main():
     print ("Y_dev shape: " + str(Y_dev.shape))
     parameters = model(X_train, Y_train, X_dev, Y_dev, num_epochs = 1)
 
-def random_mini_batches(X, Y, mini_batch_size = 64, seed = 0):
+def random_mini_batches(X, Y, mini_batch_size = 128, seed = 0):
     """
     Creates a list of random minibatches from (X, Y)
-    
+
     Arguments:
     X -- input data, of shape (input size, number of examples)
     Y -- true "label" vector (containing 0 if cat, 1 if non-cat), of shape (1, number of examples)
     mini_batch_size - size of the mini-batches, integer
     seed -- this is only for the purpose of grading, so that you're "random minibatches are the same as ours.
-    
+
     Returns:
     mini_batches -- list of synchronous (mini_batch_X, mini_batch_Y)
     """
     m = len(X)                 # number of training examples
     mini_batches = []
     np.random.seed(seed)
-    
+
     # Step 1: Shuffle (X, Y)
     permutation = list(np.random.permutation(m))
     shuffled_X = X[permutation]
@@ -63,25 +61,43 @@ def random_mini_batches(X, Y, mini_batch_size = 64, seed = 0):
         mini_batch_Y = shuffled_Y[:, k * mini_batch_size : k * mini_batch_size + mini_batch_size]
         mini_batch = (mini_batch_X, mini_batch_Y)
         mini_batches.append(mini_batch)
-    
+
     # Handling the end case (last mini-batch < mini_batch_size)
     if m % mini_batch_size != 0:
         mini_batch_X = shuffled_X[num_complete_minibatches * mini_batch_size : m]
         mini_batch_Y = shuffled_Y[:, num_complete_minibatches * mini_batch_size : m]
         mini_batch = (mini_batch_X, mini_batch_Y)
         mini_batches.append(mini_batch)
-    
+
     return mini_batches
 
 def expandMinibatchArrays(minibatch_X):
+    """
+    Expands the minibatch arrays from indices to binary vectors
+
+    Arguments:
+    minibatch_X -- training examples of the current minibatch
+    Returns:
+    mini_batch_vectors -- training expamples as binary vectors
+    """
     X = []
     vocabLength = 517430  #Look at vocab-length for this value. Everytime bag_of_words is run, this file is written to.
     for sample in minibatch_X:
         X.append(expandArray(sample, vocabLength))
     return np.array(X).T
 
-# Function looks at the dataframe and returns matrix of description encodings for all samples such that X.shape = (n_x, m)
+
 def getProductEncodingsAndPrices(df):
+    """
+    Function looks at the dataframe and returns matrix of description
+    encodings for all samples such that X.shape = (n_x, m)
+
+    Arguments:
+    df -- dataframe of data
+    Returns:
+    X -- product descriptions
+    Y -- price of item placed in pre-determined buckets
+    """
     X = []
     Y = []
     numBuckets = 12
@@ -93,12 +109,30 @@ def getProductEncodingsAndPrices(df):
     return X, Y
 
 def OneHot(bucket, numBuckets):
+    """
+    Creates onhot vector for our Y output
+
+    Arguments:
+    bucket -- index of correct bucket for example in Y
+    numBuckets -- number of buckets used to split of the prices of objects
+    Returns:
+    arr -- onehot array
+    """
     arr = np.zeros(numBuckets)
     arr[bucket] = 1
     return arr
 
-# Turns encodings of variable size (related to sentence length) to encodings of vector length corresponding to length of vocab
 def expandArray(List, vocabLength):
+    """
+    Turns encodings of variable size (related to sentence length) to encodings of
+    vector length corresponding to length of vocab
+
+    Arguments:
+    List -- list of encodings as indices
+    vocabLength -- the length of our vocabulary (corpus)
+    Returns:
+    arr -- expanded input vectors to be used in model
+    """
     arr = np.zeros(vocabLength)
     # Have to do it like this because read_csv defaults the List to really be a string that visibly looks like a list (eg. "[0 1 3]")
     for i in List.split(' '):
@@ -131,29 +165,19 @@ def model(X_train, Y_train, X_dev, Y_dev, learning_rate = 0.0001,
     costs = []                                        # To keep track of the cost
 
     # Create Placeholders of shape (n_x, n_y)
-    ### START CODE HERE ### (1 line)
     X, Y = create_placeholders(n_x, n_y)
-    ### END CODE HERE ###
 
     # Initialize parameters
-    ### START CODE HERE ### (1 line)
     parameters = initialize_parameters(n_x, m, n_y)
-    ### END CODE HERE ###
 
     # Forward propagation: Build the forward propagation in the tensorflow graph
-    ### START CODE HERE ### (1 line)
     Z2 = forward_propagation(X, parameters)
-    ### END CODE HERE ###
 
     # Cost function: Add cost function to tensorflow graph
-    ### START CODE HERE ### (1 line)
     cost = compute_cost(Z2, Y)
-    ### END CODE HERE ###
 
     # Backpropagation: Define the tensorflow optimizer. Use an AdamOptimizer.
-    ### START CODE HERE ### (1 line)
     optimizer = tf.train.AdamOptimizer(learning_rate = learning_rate).minimize(cost)
-    ### END CODE HERE ###
 
     # Initialize all the variables
     init = tf.global_variables_initializer()
@@ -174,12 +198,10 @@ def model(X_train, Y_train, X_dev, Y_dev, learning_rate = 0.0001,
                 # Select a minibatch
                 (minibatch_X, minibatch_Y) = minibatch
 
-                # IMPORTANT: The line that runs the graph on a minibatch.
+                # The line that runs the graph on a minibatch.
                 # Run the session to execute the "optimizer" and the "cost", the feedict should contain a minibatch for (X,Y).
-                ### START CODE HERE ### (1 line)
                 minibatch_X = expandMinibatchArrays(minibatch_X)
                 _ , minibatch_cost = sess.run([optimizer, cost], feed_dict = {X:minibatch_X, Y:minibatch_Y})
-                ### END CODE HERE ###
 
                 epoch_cost += minibatch_cost / num_minibatches
 
@@ -190,7 +212,7 @@ def model(X_train, Y_train, X_dev, Y_dev, learning_rate = 0.0001,
                 costs.append(epoch_cost)
 
 
-        # lets save the parameters in a variable
+        # Save the parameters in a variable
         parameters = sess.run(parameters)
         print ("Parameters have been trained!")
 
@@ -230,65 +252,54 @@ def create_placeholders(n_x, n_y):
     """
     Creates the placeholders for the tensorflow session.
     Arguments:
-    n_x -- scalar, size of an image vector (num_px * num_px = 64 * 64 * 3 = 12288)
-    n_y -- scalar, number of classes (from 0 to 5, so -> 6)
+    n_x -- scalar, size of an image vector
+    n_y -- scalar, number of classes
     Returns:
     X -- placeholder for the data input, of shape [n_x, None] and dtype "float"
     Y -- placeholder for the input labels, of shape [n_y, None] and dtype "float"
-    Tips:
-    - You will use None because it let's us be flexible on the number of examples you will for the placeholders.
-      In fact, the number of examples during test/train is different.
-    """
 
-    ### START CODE HERE ### (approx. 2 lines)
+    - Use None because it let's us be flexible on the number of examples you will for the placeholders
+    """
     X = tf.placeholder('float32', [n_x, None], name = "X")
     Y = tf.placeholder('float32', [n_y, None], name = "Y")
-    ### END CODE HERE ###
 
     return X, Y
 
 def initialize_parameters(n_x, m, n_y):
     """
     Initializes parameters to build a neural network with tensorflow. The shapes are:
-                        W1 : [25, 12288]
+                        W1 : [25, n_x]
                         b1 : [25, 1]
-                        W2 : [12, 25]
-                        b2 : [12, 1]
-                        W3 : [6, 12]
-                        b3 : [6, 1]
+                        W2 : [n_y, 25]
+                        b2 : [n_y, 1]
+
     Returns:
-    parameters -- a dictionary of tensors containing W1, b1, W2, b2, W3, b3
+    parameters -- a dictionary of tensors containing W1, b1, W2, b2
     """
 
     tf.set_random_seed(1)                   # so that your "random" numbers match ours
 
-    ### START CODE HERE ### (approx. 6 lines of code)
     W1 = tf.get_variable("W1", [25,n_x], initializer = tf.contrib.layers.xavier_initializer(seed = 1))
     b1 = tf.get_variable("b1", [25, 1], initializer= tf.zeros_initializer())
     W2 = tf.get_variable("W2", [n_y, 25], initializer = tf.contrib.layers.xavier_initializer(seed = 1))
     b2 = tf.get_variable("b2", [n_y, 1], initializer = tf.zeros_initializer())
-    #W3 = tf.get_variable("W3", [12, 12], initializer = tf.contrib.layers.xavier_initializer(seed = 1))
-    #b3 = tf.get_variable("b3", [12, 1], initializer = tf.zeros_initializer())
-    ### END CODE HERE ###
 
     parameters = {"W1": W1,
                   "b1": b1,
                   "W2": W2,
-                  "b2": b2}#,
-                  #"W3": W3,
-                  #"b3": b3}
+                  "b2": b2}
 
     return parameters
 
 def forward_propagation(X, parameters):
     """
-    Implements the forward propagation for the model: LINEAR -> RELU -> LINEAR -> RELU -> LINEAR -> SOFTMAX
+    Implements the forward propagation for the model: LINEAR -> RELU -> LINEAR -> SOFTMAX
     Arguments:
     X -- input dataset placeholder, of shape (input size, number of examples)
-    parameters -- python dictionary containing your parameters "W1", "b1", "W2", "b2", "W3", "b3"
+    parameters -- python dictionary containing your parameters "W1", "b1", "W2", "b2"
                   the shapes are given in initialize_parameters
     Returns:
-    Z3 -- the output of the last LINEAR unit
+    Z2 -- the output of the last LINEAR unit
     """
 
     # Retrieve the parameters from the dictionary "parameters"
@@ -296,16 +307,10 @@ def forward_propagation(X, parameters):
     b1 = parameters['b1']
     W2 = parameters['W2']
     b2 = parameters['b2']
-    #W3 = parameters['W3']
-    #b3 = parameters['b3']
 
-    ### START CODE HERE ### (approx. 5 lines)              # Numpy Equivalents:
     Z1 = tf.add(tf.matmul(W1, X), b1)                                              # Z1 = np.dot(W1, X) + b1
     A1 = tf.nn.relu(Z1)                                              # A1 = relu(Z1)
     Z2 = tf.add(tf.matmul(W2, A1), b2)                                              # Z2 = np.dot(W2, a1) + b2
-    #A2 = tf.nn.relu(Z2)                                              # A2 = relu(Z2)
-    #Z3 = tf.add(tf.matmul(W3, A2), b3)                                              # Z3 = np.dot(W3,Z2) + b3
-    ### END CODE HERE ###
 
     return Z2
 
@@ -314,7 +319,7 @@ def compute_cost(Z2, Y):
     """
     Computes the cost
     Arguments:
-    Z2 -- output of forward propagation (output of the last LINEAR unit), of shape (6, number of examples)
+    Z2 -- output of forward propagation (output of the last LINEAR unit)
     Y -- "true" labels vector placeholder, same shape as Z2
     Returns:
     cost - Tensor of the cost function
@@ -324,9 +329,7 @@ def compute_cost(Z2, Y):
     logits = tf.transpose(Z2)
     labels = tf.transpose(Y)
 
-    ### START CODE HERE ### (1 line of code)
     cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits = logits, labels = labels))
-    ### END CODE HERE ###
 
     return cost
 
